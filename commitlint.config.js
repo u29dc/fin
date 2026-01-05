c069709df8f89ad61d0bd1a08a0faff3948f32db
Han <han@u29dc.com>
2026-01-05 22:23:17 +0000

    style(config): clean up editorconfig, gitignore, and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,13 +2,13 @@ export default {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
 		'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'docs', 'style', 'chore', 'test']],
-		'scope-empty': [2, 'never'],
+		'type-empty': [2, 'never'],
 		'scope-enum': [2, 'always', ['web', 'core', 'cli', 'db', 'import', 'config', 'deps', 'docs', 'ci']],
+		'scope-empty': [2, 'never'],
 		'subject-empty': [2, 'never'],
-		'subject-case': [2, 'always', ['lower-case']],
-		'header-max-length': [2, 'always', 100],
+		'subject-case': [2, 'always', 'lower-case'],
 		'subject-full-stop': [2, 'never', '.'],
-		'body-max-line-length': [2, 'always', 72],
+		'header-max-length': [2, 'always', 100],
+		'body-max-line-length': [2, 'always', 100],
 	},
-	helpUrl: 'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
 };
